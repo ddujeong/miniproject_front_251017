bacbad75707787270586249850fadce326025c60
ddujeong <qoddl159@nate.com>
2025-10-21 17:51:38 +0900

    에러처리 중

--- src/component/CommentForm.js
+++ src/component/CommentForm.js
@@ -2,12 +2,14 @@ import { useNavigate } from "react-router-dom";
 import api from "../api/axiosconfig";
 import { useState } from "react";
 
-const CommentForm = ({ member, comment, loadComment, id }) => {
+const CommentForm = ({ member, loadComment, id }) => {
   const [commentText, setCommentText] = useState("");
+  const [errors, setErrors] = useState({});
   const navigate = useNavigate();
 
   const handleComment = async (e) => {
     e.preventDefault();
+    setErrors({});
     if (!commentText.trim()) {
       alert("댓글 내용을 입력해주세요.");
       return;
@@ -21,12 +23,19 @@ const CommentForm = ({ member, comment, loadComment, id }) => {
       await api.post(`/api/comment/${id}`, { content: commentText });
       alert("댓글 작성 완료");
       loadComment();
-    } catch (error) {
-      console.error(error);
+    } catch (err) {
+      if (err.response && err.response.status === 400) {
+        setErrors(err.response.data);
+      } else {
+        alert("댓글 작성 중 오류가 발생했습니다.");
+      }
     }
   };
   return (
     <div className="comment-form">
+      {errors.content.content && (
+        <p className="err">{errors.content.content}</p>
+      )}
       <input
         className="comment-input"
         placeholder={member ? "댓글 작성..." : "로그인 후 댓글 작성 해주세요"}
@@ -34,6 +43,7 @@ const CommentForm = ({ member, comment, loadComment, id }) => {
         value={commentText}
         onChange={(e) => setCommentText(e.target.value)}
       />
+
       <button
         className="comment-button"
         onClick={handleComment}
